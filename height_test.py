08f876609a2f315d04c41064d0ac75f7726e49d5
Pankaj Gautam <psgautam@gmail.com>
2026-01-21 16:55:58 -0600

    Add height comparison program to determine if user is taller than average

---
+++ height_test.py
@@ -0,0 +1,10 @@
+# This program asks the user for their height in inches
+height = input("How tall are you in inches?")
+height = int(height)
+if height >= 64:
+    print ("You are taller than the average!")
+else:
+    print("You are shorter than the average!")
+
+ 
+    
